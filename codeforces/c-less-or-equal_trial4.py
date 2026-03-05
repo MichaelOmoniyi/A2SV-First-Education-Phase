7b5132178ca6b7b686461d931250556256ac255b
MichaelOmoniyi <101010436+MichaelOmoniyi@users.noreply.github.com>
2026-03-05 22:08:51 +0100

    Add solution for C - Less or Equal (trial 4)

---
+++ codeforces/c-less-or-equal_trial4.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+# Exactly zero elements must be less than x
+# x must be less than all the element but within the range provided
+if k == 0:
+    if arr[0] < 2:
+        print(-1)
+    else:
+        print(arr[0] - 1)
+elif k == n: # Exactly k elements must be less than or equal to x which is the max number
+    if arr[-1] > 10**9:
+        print(-1)
+    else:
+        print(arr[-1])
+else:
+    if arr[k - 1] == arr[k]:
+        print(-1)
+    else:
+        if arr[k - 1] > 10**9:
+            print(-1)
+        else:
+            print(arr[k - 1])
